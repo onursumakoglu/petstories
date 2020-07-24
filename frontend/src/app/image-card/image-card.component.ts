cd56f91ef172ea3a4b13143f1fcf44adda2178d9
Onur Sumakoglu <sumakogluonur@gmail.com>
2020-07-24 16:37:08 +0300

    added like button

---
+++ frontend/src/app/image-card/image-card.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-image-card',
+  templateUrl: './image-card.component.html',
+  styleUrls: ['./image-card.component.scss']
+})
+export class ImageCardComponent implements OnInit {
+
+  @Input() image;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  liked = false;
+
+  like(){
+    this.liked = !this.liked;
+  }
+
+}
--- frontend/src/app/image-list/image-list.component.ts
+++ frontend/src/app/image-list/image-list.component.ts
@@ -16,4 +16,6 @@ export class ImageListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  
+
 }
